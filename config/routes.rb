baf31d848b2875a3efe2348a47ed907508d663b1
naumenko <mobilesoft@spaces.ru>
2015-02-07 14:07:53 +0200

    add main routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :codes
+  resources :code_types
+  resources :orders
+  resources :requests
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
