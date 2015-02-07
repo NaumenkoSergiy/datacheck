83a7fc606c09c1c4a46b0c2aff25eae09ba9238a
naumenko <mobilesoft@spaces.ru>
2015-02-07 13:00:09 +0200

    add Order model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207104600) do
+ActiveRecord::Schema.define(version: 20150207105625) do
+
+  create_table "orders", force: true do |t|
+    t.string  "number"
+    t.integer "count"
+    t.integer "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
