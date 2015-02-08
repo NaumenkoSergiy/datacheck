661231c42da57f1325581db4a0483e7ce3748d5b
naumenko <mobilesoft@spaces.ru>
2015-02-08 16:24:26 +0200

    create code if create order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :codes
+
+  after_create :create_code
+
+  private
+
+  def create_code
+    codes.create(code: rand(999999999999))
+  end
 end
