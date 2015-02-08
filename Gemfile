d1747cfa27b5fa603b01c4766eb830fddabfaa62
naumenko <mobilesoft@spaces.ru>
2015-02-08 10:41:15 +0200

    add admin

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'spring',        group: :development
 
 gem 'devise'
 gem 'pry'
+gem 'pg'
 
 gem 'twitter-bootstrap-rails'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :determine_layout
+
+  helper_method :is_admin?
+  def is_admin?
+    current_user.try(:admin)
+  end
+
+  private
+
+  def determine_layout
+    is_admin? ? 'admin' : 'application'
+  end
 end
